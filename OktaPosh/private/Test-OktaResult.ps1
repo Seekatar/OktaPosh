bdca431ca2d3c164e221e2412fab935c94c024d7
Jim Wallace <seekatar@gmail.com>
2020-10-31 17:26:01 -0400

    update build

--- OktaPosh/private/Test-OktaResult.ps1
+++ OktaPosh/private/Test-OktaResult.ps1
@@ -60,6 +60,7 @@ function Test-OktaResult {
     } else {
         # 429 is rate limit, 20-100/minute depending on the request
         # https://developer.okta.com/docs/reference/rate-limits/
+        #
         $oktaError = $result
         try {
             $parms = @{}
